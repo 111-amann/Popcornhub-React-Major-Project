07776f0cb905bf54373eb93da796f0fe346b4fa4
111-amann <amansayyad7887@gmail.com>
2025-02-23 21:32:54 +0530

    moviedetails page navigation completed

--- src/Components/Moviedetails.jsx
+++ src/Components/Moviedetails.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { asyncloadmovie, removemovie } from "../store/actions/movieActions";
+import { useNavigate, useParams } from "react-router-dom";
+import Loading from "./Loading";
 
 const Moviedetails = () => {
-  return (
-    <div>
-      Moviedetails
-    </div>
-  )
-}
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { info } = useSelector((state) => state.movie);
+  console.log(info);
 
-export default Moviedetails
+  useEffect(() => {
+    dispatch(asyncloadmovie(id));
+    return () => {
+      dispatch(removemovie());
+    };
+  }, []);
+  return info ? (
+    <div
+      style={{
+        background: `linear-gradient(rgba(0, 0, 0,.2),rgba(0, 0, 0,.3),rgba(0, 0, 0,.6)), url(https://image.tmdb.org/t/p/original/${
+          info.detail.backdrop_path || info.detail.poster_path
+        })`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+      className="w-screen h-screen md:px-5 px-1"
+    >
+      <nav className="w-full text-zinc-200 flex md:gap-10 sm:gap-5 gap-3 items-center sm:text-md md:text-lg text-sm drop-shadow-lg bg-transparent bg-white/10 sm:mt-4 mt-2 py-2 px-2">
+        <i
+          onClick={() => navigate("/")}
+          className="ri-arrow-left-line bg-gray-800 text-white p-1 md:p-2 rounded-full lg:shadow-lg md:shadow-md hover:bg-yellow-500 hover:drop-shadow-2xl transition lg:mr-5 md:mr-2 mr-1 sm:text-md md:text-lg text-sm static"
+        ></i>
+        <a target="_blank" href={info.detail.homepage}>
+          <i class="ri-external-link-fill hover:text-zinc-100 hover:drop-shadow-2xl"></i>
+        </a>
+        <a
+          target="_blank"
+          href={`https://www.wikidata.org/wiki/${info.externalid.wikidata_id}/`}
+        >
+          <i class="ri-earth-fill hover:text-zinc-100 hover:drop-shadow-2xl"></i>
+        </a>
+        <a
+          class="hover:text-zinc-100 hover:drop-shadow-2xl"
+          target="_blank"
+          href={`https://www.imdb.com/title/${info.externalid.imdb_id}/`}
+        >
+          imdb
+        </a>
+      </nav>
+    </div>
+  ) : (
+    <Loading />
+  );
+};
+
+export default Moviedetails;
